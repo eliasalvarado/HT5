3934e0d246562c18d927f20a07bb87ae06593534
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2022-03-13 22:42:16 -0600

    Se terminó el programa

--- SimuladorRam.py
+++ SimuladorRam.py
@@ -1,2 +1,78 @@
+'''
+Autor: Elias Alvarado - 21808
+Fecha: 13/03/2022
+Simulador de memoria RAM con distinta cantidad de procesos los cuales tendran distintas instrucciones por completar.
+Se utilizara para poder medir el tiempo utilizado en distintos escenarios.
+'''
+
 import simpy
 import random
+
+def proceso(noProceso, env, ram, procesadores, llegada, numInstruc, solicitud, velocidad):
+    global tiempoTotal
+    global tiempos
+    
+    yield env.timeout(llegada)
+    llegadaProceso = env.now
+    print(f"[NEW] - Proceso No.{noProceso} llego en {llegada} y solicita {solicitud} de memoria RAM. |{ram.level} espacio disponible|")
+    yield ram.get(solicitud)
+    print(f"[ADMITTED] - Proceso No.{noProceso} recibio {solicitud}. |{ram.level} espacio disponible|")
+    
+    while numInstruc > 0:
+        print(f"[READY] - Proceso No.{noProceso} tiene {numInstruc} pendientes.")
+        
+        with procesadores.request() as req:
+            yield req
+            finalizadas = velocidad
+            numInstruc -= finalizadas
+            yield env.timeout(1)
+            if numInstruc < 0:
+                numInstruc = 0
+            print(f"[RUNNING] - Se han ejecutado {finalizadas} instrucciones del Proceso No.{noProceso} tiene {numInstruc} pendientes. |{ram.level} espacio disponible|")
+            
+            if numInstruc > 0:
+                watting = random.randint(1,2)
+                if watting == 1:
+                    print(f"[WATTING] - Proceso No.{noProceso} realizando operaciones I/O.")
+                    yield env.timeout(1)
+    yield ram.put(solicitud)
+    print(f"[TERMINATED] - Proceso No.{noProceso} termino sus instrucciones en {env.now}s, se dispone de {solicitud} mas de memoria. |{ram.level} espacio disponible|")
+    tiempos.append(env.now - llegada)
+    tiempoTotal += (env.now - llegada)
+    print("Proceso No.{:.0f} tardo {:.2f}s.".format(noProceso,env.now - llegada))
+
+random.seed(100)
+
+tiempoTotal = 0
+tiempos = []
+#------------------
+numProcesos = 25
+velocidad = 3
+capacidadRam = 100
+numProcesadores = 1
+intervalos = 10
+#-----------------
+env = simpy.Environment()
+ram = simpy.Container(env, init = capacidadRam, capacity = capacidadRam)
+procesadores = simpy.Resource(env, capacity = numProcesadores)
+
+for noProceso in range(numProcesos):
+    llegada = random.expovariate(1.0 / intervalos)
+    numInstruc = random.randint(1, 10)
+    solicitud = random.randint(1, 10)
+    env.process(proceso(noProceso, env, ram, procesadores, llegada, numInstruc, solicitud, velocidad))
+
+env.run()
+
+tiempoPromedio = (tiempoTotal / numProcesos)
+suma = 0
+for i in tiempos:
+    suma += (i - tiempoPromedio)**2
+desviacionEst = (suma/(numProcesos-1))**0.5
+print("---------------------INFORMACION------------------------------")
+print("\tTiempo promedio: {:.2f} segundos".format(tiempoPromedio))
+print("\tDesviacion Estandar: {:.2f} segundos".format(desviacionEst))
+
+
+
+
